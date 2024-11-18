93f0043e9896b38574c9a0018d2633aff8825484
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-18 22:03:50 +0100

    initial setup + created arraylist and list

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+
+namespace minioppgave_arraylist_and_lists;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayList arrayList = new ArrayList();
+        arrayList.Add("Hello there!");
+        arrayList.Add("General Kenobi!");
+        arrayList.Add(1);
+        arrayList.Add(true);
+        arrayList.Add(string.Empty);
+
+        foreach (var myObjects in arrayList)
+        {
+            Console.WriteLine(myObjects);
+        }
+
+        int num1 = 62;
+        Console.WriteLine(num1);
+
+        List<object> myList = new List<object>();
+        myList.Add("Hello there!");
+        myList.Add("General Kenobi!");
+        myList.Add(23);
+        myList.Add(false);
+        myList.Add(string.Empty);
+
+        foreach (var otherObjects in myList)
+        {
+            Console.WriteLine(otherObjects);
+        }
+
+
+    }
+}
